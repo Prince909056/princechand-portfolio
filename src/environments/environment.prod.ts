96b261628641af1adf625418f1ef34d70a4a77ae
Prince Chand <87266488+Prince909056@users.noreply.github.com>
2026-08-16 21:30:58 +0530

    Configure production fileReplacements mapping in angular.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  yearsOfExperience: '3.8+',
-  starOfTheMonthLink: 'https://linkedin.com',
-  risingStarLink: 'https://linkedin.com',
-  cleanCodeCertLink: 'https://udemy.com',
-  linkedInUrl: 'https://linkedin.com',
+  yearsOfExperience: '3.8',
+  starOfTheMonthLink: 'https://www.linkedin.com/posts/anviamsolutions_anviamrockstars-aboveandbeyond-teamanviam-activity-7345398424978001921-TGYZ/?utm_source=share&utm_medium=member_desktop&rcm=ACoAADI--QUBFqx3OIYgf8sBrrYLaIE75o5D_ik',
+  risingStarLink: 'https://www.linkedin.com/feed/update/urn:li:activity:7154809121123274752/',
+  cleanCodeCertLink: 'https://www.udemy.com/certificate/UC-b4255959-f5c5-4308-9a21-88a8ff4ac0f4/',
+  linkedInUrl: 'https://www.linkedin.com/in/prince-chand-1993941b6/',
   emailAddress: 'prince.chand.dev@gmail.com',
   phoneNumber: '(+91) 9888725336'
 };
